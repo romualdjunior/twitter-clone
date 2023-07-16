9498d283950f8985852b3c78c228ec0d803abfef
romualdjunior <romuald.motchehokamguia@esprit.tn>
2023-07-16 19:04:27 +0200

    changing allowed-hosts

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,12 +25,7 @@ SECRET_KEY = "*n*f0zqlu+cgn&m*sd3^qkz9b=nw6db(tfo$156o9ag-%2f_eq"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    "127.0.0.1",
-    "localhost",
-    "twitter-app-nrhdf.ondigitalocean.app",
-    ".vercel.app",
-]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
